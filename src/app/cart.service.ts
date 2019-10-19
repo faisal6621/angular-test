267a959d5f90557ba2f4d2facd4fdd71c0830bd8
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-10-19 21:26:37 +0530

    added shipping component and HttpClientModule

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
   items = []
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   addToCart(product) {
     this.items.push(product)
@@ -19,5 +25,8 @@ export class CartService {
     this.items = []
     return this.items
   }
-  constructor() { }
+
+  getShippingPrices() {
+    return this.httpClient.get('/assets/shipping.json')
+  }
 }
